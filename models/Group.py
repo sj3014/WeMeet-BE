7ac0f19f0f4c357ebd87cd758beb0bb5f1f0c31f
Daniel Kim <kdanielive@gmail.com>
2022-11-13 18:37:29 -0500

    Changed so that it works with flask_sqlalchemy

--- models/Group.py
+++ models/Group.py
@@ -1,13 +1,12 @@
-import sqlalchemy as db
-from sqlalchemy.ext.declarative import declarative_base
+from flask_sqlalchemy import SQLAlchemy
 
-class Group(declarative_base()):
-    __name__ = 'group'
+db = SQLAlchemy()
+
+class Group(db.Model):
+    __tablename__ = 'group'
 
     group_id = db.Column(db.String(25), primary_key=True)
     group_name = db.Column(db.String(25))
 
 if __name__ == "__main__":
-    print(
-        Group.__table__
-    )
+    pass
