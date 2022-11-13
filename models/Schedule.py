12c603a26d2b94886c88bda2979532a8bf645a5a
jackie-vifive <106610327+jackie-vifive@users.noreply.github.com>
2022-11-13 18:38:29 -0500

    Schedule model

---
+++ models/Schedule.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+db = SQLAlchemy()
+
+
+class Schedule(db.Model):
+    __tablename__ = 'Schedule'
+
+    schedule_id = db.Column(db.String(25), primary_key=True)
+    schedule_name = db.Column(db.String(25))
+    start_time = db.Column(db.DateTime, default=datetime.now())
+    end_time = db.Column(db.DateTime, default=datetime.now())
+    user_id = db.Column(db.String(25))
+
+
+if __name__ == "__main__":
+    print(Schedule.schedule_id)
+
