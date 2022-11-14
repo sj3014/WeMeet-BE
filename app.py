c37dbb6d312ea43006e2c38dbd74e1c96b705733
Seong Moon Jo <sj3014@columbia.edu>
2022-11-13 19:45:54 -0500

    db connected

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from routes.user_bp import user_bp
-
-db = SQLAlchemy()
+from models.database import db
+from models import *
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
     db.init_app(app)
-
     with app.app_context():
         app.register_blueprint(user_bp, url_prefix='/users')
         db.create_all()
