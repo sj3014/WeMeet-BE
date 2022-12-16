c193fe87573467527dca906da92a250c12f47dfd
jackie-vifive <106610327+jackie-vifive@users.noreply.github.com>
2022-12-16 00:23:58 -0500

    Change schedule routes

--- routes/schedule_routes.py
+++ routes/schedule_routes.py
@@ -3,6 +3,6 @@ from controllers.schedule_controller import *
 
 schedule_routes = Blueprint('schedule_routes', __name__)
 
-schedule_routes.route('/list_schedule', methods=['GET'])(list_schedules)
-schedule_routes.route('/create_schedule', methods=['POST'])(create_schedule)
+schedule_routes.route('/', methods=['GET'])(list_schedules)
+schedule_routes.route('/', methods=['POST'])(create_schedule)
 
