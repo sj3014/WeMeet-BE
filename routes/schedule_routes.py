20b9638eee8208a00217adbfa1066d03bafba889
jackie-vifive <106610327+jackie-vifive@users.noreply.github.com>
2022-12-16 01:49:13 -0500

    Add CORS

--- routes/schedule_routes.py
+++ routes/schedule_routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from controllers.schedule_controller import *
+from flask_cors import CORS
 
 schedule_routes = Blueprint('schedule_routes', __name__)
+CORS(schedule_routes)
 
 schedule_routes.route('/', methods=['GET'])(list_schedules)
 schedule_routes.route('/', methods=['POST'])(create_schedule)
